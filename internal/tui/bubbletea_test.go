cdda40be1746496d59cfb5d508f848106e6eea1e
Emil Oszmánbegovity <emil@emilosman.com>
2025-08-19 11:43:58 +0200

    Create empty tui tests

---
+++ internal/tui/bubbletea_test.go
@@ -0,0 +1,13 @@
+package tui
+
+import "testing"
+
+var ()
+
+func TestTui(t *testing.T) {
+	t.Run("Should build feeds list", func(t *testing.T) {
+	})
+
+	t.Run("Should build items list", func(t *testing.T) {
+	})
+}
